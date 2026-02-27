9437ebd7b8265d1d7e11d8043e93ccc8bd570295
AndrewBlack21 <andrew.moraes16@hotmail.com>
2026-02-27 14:35:07 -0300

    "Fix: corrigindo problema no main"

--- backend/main.py
+++ backend/main.py
@@ -5,23 +5,23 @@ from scraper import get_product_data
 
 app = FastAPI()
 
-app.data_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_methods=["*"],
-  allow_headers=["*"],
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 class ProductRequest(BaseModel):
     url: str
+
 
 @app.post("/mercadolivre")
 def mercadolivre(data: ProductRequest):
     try:
         product = get_product_data(data.url)
-
-
-        return{
+        return {
             "title": product["title"],
             "price": product["price"],
             "affiliateLink": product["final_url"],
